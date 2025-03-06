144971ff07729d48844972698526098c0d471991
Neil Al Hakim <nal-haki@c1r4s3.42beirut.com>
2025-03-06 15:11:51 +0200

    teeeeeeeeeeeeeest

--- srcs/cub3d.c
+++ srcs/cub3d.c
@@ -6,7 +6,7 @@
 /*   By: nal-haki <nal-haki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 14:33:15 by nal-haki          #+#    #+#             */
-/*   Updated: 2024/12/18 19:06:15 by nal-haki         ###   ########.fr       */
+/*   Updated: 2025/03/06 15:11:11 by nal-haki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,9 @@ int	main(int ac, char **av)
 	t_config	scene_data;
 
 	if (ac != 2)
-		return (write(2, "Error\nusage : ./cub3d (map_in_format.cub)\n", 42)
+	return (write(2, "Wrong arguments!\n", 18)
 			, 1);
 	file_parser(&scene_data, av[1]);
 	get_start(&scene_data);
+	
 }
